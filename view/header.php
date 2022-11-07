a93206c7542b56188c8ab990242ee601d3f9b4ff
SandalikaSC <sandalikachamari@gmail.com>
2022-11-07 19:24:22 +0530

    login

--- view/header.php
+++ view/header.php
@@ -7,24 +7,34 @@
     <title>Page Title</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='../view/css/header.css'>
-    <script src='main.js'></script>
+    
 </head>
 
 <body>
-    <img class="vogue" src="assets/logo.png">
-    <nav class="nav">
-
-        <div class="profile">
-            <img class="profileImg center" src="assets/pp.png" alt="pp">
-            <h4 class="user-name">Nirmani Abesinghe</h4>
+<div class="all">
+        <img class="vogue" src="assets/logo.png" alt="logo">
+        <div class="toggler" id="toggler">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
         </div>
+        <nav class="nav" id="nav">
+            <div class="old">
+                <div class="profile">
+                    <img class="profileImg center" src="assets/pp.png" alt="pp">
+                    <h4 class="user-name">Nirmani Abesinghe</h4>
+                </div>
 
-        <?php include "components/dashboardContainer.html" ?>
-        <div class="logout-btn inactive-btn center ">
-            <h4 class="page-btn-text gold-text">Logout</h4>
-        </div>
+                <?php include 'components/dashboardContainer.html' ?>
 
-    </nav>
+                <div class="logout-btn inactive-btn center ">
+                    <h4 class="page-btn-text gold-text">Logout</h4>
+                </div>
+            </div>
+        </nav>
+        
+    </div>
+    <script src='js/header.js'></script>
 </body>
 
 </html>
