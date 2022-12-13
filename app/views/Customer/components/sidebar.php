545bad340a1ae497809533e08a841e3146742371
SandalikaSC <sandalikachamari@gmail.com>
2022-12-13 12:27:46 +0530

    sidebar

--- app/views/Customer/components/sidebar.php
+++ app/views/Customer/components/sidebar.php
@@ -6,7 +6,7 @@
         <div class="bar"></div>
     </div>
     <nav class="nav" id="nav">
-        <div class="old">
+         
             <div class="profile">
                 <img class="profileImg center" src="<?php echo URLROOT ?>/img/pp.png" alt="pp">
                 <h4 class="user-name">
@@ -14,6 +14,8 @@
                 </h4>
             </div>
             <div class="container">
+            <br>
+            <br>
                 <div class="activate page-btn">
                     <img class="page-btn-img" src="<?php echo URLROOT ?>/img/home-gold.png">
                     <h4 class="page-btn-text gold-text">Dashboard</h4>
@@ -43,9 +45,9 @@
                 </div>
             </div>
             <div class="logout-btn inactive-btn center ">
-                <h4 class="page-btn-text gold-text"><a href="<?php echo URLROOT; ?>/Login/logout"> Logout</a></h4>
+                <h4 class="page-btn-text gold-text"><a class="a" href="<?php echo URLROOT; ?>/Login/logout"> Logout</a></h4>
             </div>
-        </div>
+         
     </nav>
 
 </div>
