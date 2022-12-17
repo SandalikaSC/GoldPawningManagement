ad4a5019dd43720f46c30e08f148e81962eaf33b
SandalikaSC <sandalikachamari@gmail.com>
2022-12-17 15:09:13 +0530

    Update Users.php

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -273,13 +273,6 @@ class Users extends Controller
     redirect('/Users');
   }
 
-  public function isLoggedIn()
-  {
-    if (isset($_SESSION['user_id'])) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+ 
 
 }
