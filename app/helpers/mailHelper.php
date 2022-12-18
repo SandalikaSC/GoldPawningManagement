658e5b8c0dacf57b9c2b567ab67e1f87de484eda
SandalikaSC <sandalikachamari@gmail.com>
2022-12-18 14:01:00 +0530

    email case

--- app/helpers/mailHelper.php
+++ app/helpers/mailHelper.php
@@ -41,15 +41,15 @@ function sendMail($receiver, $email_Type, $code, $name)
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
 
-        $mail->Username   = 'voguepawners@gmail.com';                     //SMTP username
-        // $mail->Password   = MAIL_PASSWORD;                               //SMTP password
+        $mail->Username   = EMAIL;                     //SMTP username
+        $mail->Password   = MAIL_PASSWORD;                               //SMTP password
 
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
         //Recipients
 
-        $mail->setFrom('voguepawners@gmail.com', 'vogue');
+        $mail->setFrom(EMAIL, 'Vogue');
         //Add a recipient
 
         //Content
