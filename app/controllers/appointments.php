8dea2c1dc048cd4891372caeac3041458a36115e
SandalikaSC <sandalikachamari@gmail.com>
2022-12-19 19:05:00 +0530

    view add appointment

--- app/controllers/appointments.php
+++ app/controllers/appointments.php
@@ -14,7 +14,7 @@ class appointments extends Controller
 
         public function index()
         {
-
+                $this->viewAppointments();
 
         }
         public function loadTimeSlots()
@@ -23,27 +23,80 @@ class appointments extends Controller
                         $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
 
+                        $result = $this->Model->getSlotsNotIn(trim($_POST['date']));
+
                         $data = [
+                                'reasonID' => trim($_POST['reason']),
                                 'date' => trim($_POST['date']),
                                 'date_err' => '',
-                                'time_slots' => $_POST['selector'],
-                                'time_slot_err' => '',
-                                'reason' => $_POST['reason'],
-                                'reason_err' => ''
-                        ];
-                        echo $data['time_slots'];
-                        echo $data['reason'];
-
-                        $result = $this->Model->getSlotsNotIn($data['date']);
-                        // Init data
-                        $data = [
-                                'date' => trim($_POST['date']),
-                                'date_err' => '',
-                                'time_slots' => $result
+                                'time_slots' => $result,
+                                'time_slot_err' => ''
 
                         ];
+
+                        if (empty($data['date'])) {
+                                $data['date_err'] = 'Required Field';
+                                $data['time_slots'] = (array) null;
+
+                        } else if ($data['date'] < date("Y-m-d")) {
+                                $data['date_err'] = 'invalid date';
+                                $data['time_slots'] = (array) null;
+
+                        } else if (empty($data['time_slots'])) {
+                                $data['time_slot_err'] = 'No time slots available';
+                        }
+
+
                         // $this->view('pages/userLogin', $data);
                         $this->view('Customer/newAppointment', $data);
+
+
+                } else if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
+                        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+                        $data = [
+                                'reasonID' => trim($_POST['reason']),
+                                'date' => trim($_POST['date']),
+                                'date_err' => '',
+                                'time_slots' => '',
+                                'time_slot_err' => ''
+
+                        ];
+                        if (!empty(trim($_POST['selector']))) {
+                                $data['time_slots'] = trim($_POST['selector']);
+
+                        }
+
+                        if (empty($data['date'])) {
+                                $data['date_err'] = 'Required Field';
+
+                        } else if ($data['date'] < date("Y-m-d")) {
+                                $data['date_err'] = 'invalid date';
+                                $data['time_slots'] = (array) null;
+
+                        }
+
+                        if (empty($data['time_slots'])) {
+                                $data['time_slot_err'] = 'Required Field';
+
+                        }
+                        if (!empty($data['reasonID']) && empty($data['date_err']) && empty($data['time_slot_err'])) {
+                                // echo 'submitted';
+                                $result = $this->Model->addAppointment($data);
+                                if ($result) {
+                                        $date = $data['date'];
+                                        flash('appointment', "Your Appointment on '{$date}'  is Confirmed.", 'success');
+                                        redirect('/appointments');
+                                } else {
+                                        flash('appointment', 'Something went wrong. Try Again.', 'invalid');
+
+                                }
+
+                        } else {
+                                $this->view('Customer/newAppointment', $data);
+                        }
+
+
 
 
                 }
@@ -65,16 +118,52 @@ class appointments extends Controller
                         $result = $this->Model->getSlotsNotIn(date('Y-m-d'));
                         // Init data
                         $data = [
+                                'reasonID' => "1",
                                 'date' => date('Y-m-d'),
                                 'date_err' => '',
-                                'time_slots' => $result
-
+                                'time_slots' => $result,
+                                'time_slot_err' => ''
                         ];
 
+                        if (empty($data['time_slots'])) {
+                                $data['time_slot_err'] = 'No time slots available';
+                        }
 
                         // $this->view('pages/userLogin', $data);
                         $this->view('Customer/newAppointment', $data);
                 }
+        }
+
+        public function viewAppointments()
+        {
+
+                if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['cancel'])) {
+                        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+
+                        // $result = $this->Model->getSlotsNotIn(trim($_POST['date']));
+
+                        $data = [];
+                } elseif ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['deupdatelete'])) {
+                        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+
+                        // $result = $this->Model->getSlotsNotIn(trim($_POST['date']));
+
+                        $data = [];
+                } else {
+                        $result = $this->Model->getAppointmentById($_SESSION['user_id']); 
+                        $data = [
+                                'appointments' => $result
+                        ];
+                       
+                        if (empty($result)) {
+                                $data['appointments'] = (array) null;
+                        }
+
+                        $this->view('Customer/appointments', $data);
+                }
+
         }
 }
 ?>
--- app/views/Customer/newAppointment.php
+++ app/views/Customer/newAppointment.php
@@ -8,7 +8,7 @@
     <div class="">
         <div class="right-heading">
             <div class="right-side">
-                <a href="<?php echo URLROOT ?>/customerDashboard/appointment" id="back">
+                <a href="<?php echo URLROOT ?>/appointments" id="back">
                     <img class="back" src="<?php echo URLROOT ?>/img/back.png" alt="back">
                 </a>
                 <h1 id="title">Make Appointment</h1>
@@ -18,16 +18,22 @@
 
     </div>
     <div class="content">
+    <div>
+        
+    </div>
+        <div class="hidden"> <?php flash('appointment'); ?>  </div>
         <form action="<?php echo URLROOT ?>/appointments/loadTimeSlots" method="POST" class="addForm">
+       
+        
             <label for="Reason" class="label">Reason</label>
             <div class="form-input">
                 <!-- <input type="text" class="input"> -->
                 <select name="reason" id="Reason" class="input">
-                    <option class="input" value="1">Pawn Article</option>
-                    <option class="input" value="2">Make payment</option>
-                    <option class="input" value="3">Reserve Locker</option>
-                    <option class="input" value="4">Renew pawning</option>
-                    <option class="input" value="5">Retrieve article</option>
+                    <option class="input" value="1" <?php echo($data['reasonID']==1)? 'selected':''?>>Pawn Article</option>
+                    <option class="input" value="2" <?php echo($data['reasonID']==2)? 'selected':''?>>Make payment</option>
+                    <option class="input" value="3" <?php echo($data['reasonID']==3)? 'selected':''?>>Reserve Locker</option>
+                    <option class="input" value="4" <?php echo($data['reasonID']==4)? 'selected':''?> >Renew pawning</option>
+                    <option class="input" value="5" <?php echo($data['reasonID']==5)? 'selected':''?>>Retrieve article</option>
 
                 </select>
                 <span></span>
@@ -35,42 +41,59 @@
             <label class="label">Choose Date</label>
             <div class="form-input ">
                 <div class=" search-div">
-                    <input type="date" name="date" class="input" value="<?php echo $data['date']; ?>">
+                    <input type="date" name="date" id="date" class="input" value="<?php echo $data['date']; ?>">
                     <button name="search" class="search">Search</button>
 
 
                 </div>
-                <span></span>
+                <span id="date_err"> <?php echo $data['date_err']; ?></span>
             </div>
             <label class="label">Choose Time slot</label>
-            <div class="form-input">
+            <div id="slots" class="form-input">
                 <!-- <input type="text" class="input"> -->
-                <?php
-            foreach ($data['time_slots'] as $timeslot): ?>
+                 
+                <?php foreach ($data['time_slots'] as $timeslot): ?>
                 <div class=" selecotr-item">
-                    <input type="radio" id=<?php echo $timeslot->slotID ?> name="selector" class="selector-item_radio"
-                    checked>
+                    <input type="radio" value="<?php echo $timeslot->slotID ?>" id=<?php echo $timeslot->slotID ?>
+                    name="selector" class="selector-item_radio" checked
+                    >
                     <label for=<?php echo $timeslot->slotID ?> class="selector-item_label">
                         <?php echo $timeslot->time ?>
                     </label>
                 </div>
                 <?php endforeach; ?>
-
-                <span></span>
+                
+                <span><?php echo $data['time_slot_err']; ?></span>
 
             </div>
             <div></div>
-            <!--          
+            <!--                      
             <a id="dashboard" class="cancel-btn" href="<?php echo URLROOT ?>/customerDashboard/appointment"> 
             Cancel
         </a>  -->
+
             <div>
-                <input type="button" class="button submit" value="Submit">
+                <!-- <button type="button" id="submit" name="submit" class="button submit" value="Submit"></button> -->
+                <button id="submit" name="submit" class="<?php echo(!empty($data['time_slot_err']))? 'none':'button submit'?>" value="" >Submit</button>
             </div>
 
         </form>
         <div class="dbload">
+        
             <img src="<?php echo URLROOT ?>/img/Calendar-amico.png" alt="" class="appointment">
         </div>
     </div>
+    <script>
+
+        document.getElementById("date").addEventListener("change", hideTimeslot);
+
+        function hideTimeslot() {
+
+            document.getElementById("slots").style.display = 'none';
+            document.getElementById("submit").style.display = 'none';
+
+        }
+
+
+    </script>
     <?php require APPROOT . "/views/inc/footer.php" ?>
