eeb5b5d4a94657a35a28fc0a9e54370cf4c6f98b
SandalikaSC <66319075+SandalikaSC@users.noreply.github.com>
2023-02-02 19:29:31 +0530

    landing page FAQ

--- app/views/pages/VogueLanding.php
+++ app/views/pages/VogueLanding.php
@@ -2,196 +2,215 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <title>Vogue Pawn</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link rel="icon" type="image/x-icon" href="<?php echo URLROOT ?>/Img/logo.png">
-  <link rel="stylesheet" href='<?php echo URLROOT ?>/css/landing.css' />
+    <meta charset="UTF-8" />
+    <title>Vogue Pawn</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" type="image/x-icon" href="<?php echo URLROOT ?>/Img/logo.png">
+    <link rel="stylesheet" href='<?php echo URLROOT ?>/css/landing.css' />
 </head>
 
 <body>
-  <!-- Header Start -->
-  <header class="site-header">
-    <div class="wrapper site-header__wrapper">
-      <div class="site-header__start">
-        <div><img src='<?php echo URLROOT ?>/img/logo.png' alt="" class="logo"></div>
-        <div><img src='<?php echo URLROOT ?>/img/halflogo.png' alt="" class="halflogo"></div>
-        <!-- <a href="#" class="brand">Brand</a> -->
-      </div>
-      <div class="site-header__middle">
-        <nav class="nav">
-          <button class="nav__toggle" aria-expanded="false" type="button">
-            menu
-          </button>
-          <ul class="nav__wrapper">
-            <li class="nav__item active"><a href="#">Home</a></li>
-            <li class="nav__item"><a href="#">Services</a></li>
-            <li class="nav__item"><a href="#">About</a></li>
+    <!-- Header Start -->
+    <header class="site-header">
+        <div class="wrapper site-header__wrapper">
+            <div class="site-header__start">
+                <div><img src='<?php echo URLROOT ?>/img/logo.png' alt="" class="logo"></div>
+                <div><img src='<?php echo URLROOT ?>/img/halflogo.png' alt="" class="halflogo"></div>
+                <!-- <a href="#" class="brand">Brand</a> -->
+            </div>
+            <div class="site-header__middle">
+                <nav class="nav">
+                    <button class="nav__toggle" aria-expanded="false" type="button">
+                        menu
+                    </button>
+                    <ul class="nav__wrapper">
+                        <li class="nav__item active"><a href="#">Home</a></li>
+                        <li class="nav__item"><a href="#">Services</a></li>
+                        <li class="nav__item"><a href="#">About</a></li>
 
-            <li class="nav__item"><a href="#">FAQ</a></li>
-          </ul>
-        </nav>
-      </div>
-      <div class="site-header__end">
-        <a href="<?php echo URLROOT ?>/Users/login"> <button class="signin"> Sign in</button></a>
-      </div>
-    </div>
-  </header>
-  <section class="top1">
-    <img class="top1Img" src="<?php echo URLROOT ?>/img/Picture1.png">
-    <div class="top1Content">
+                        <li class="nav__item"><a href="#">FAQ</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="site-header__end">
+                <a href="<?php echo URLROOT ?>/Users/login"> <button class="signin"> Sign in</button></a>
+            </div>
+        </div>
+    </header>
+    <section class="top1">
+        <img class="top1Img" src="<?php echo URLROOT ?>/img/Picture1.png">
+        <div class="top1Content">
 
-      <h2 class="sectiontopic">INSTANT CASH FOR <br> YOUR <b><i><span class="gold">GOLD!</span></i></b> </h2>
-      <h3 class="registration">New Here ?</h3>
-      <h3 class="reg-txt">Sign up and discover great amount of new opportunities!</h3>
-      <a href="<?php echo URLROOT ?>/Users/signup"> <button class="signup"> Sign Up >></button></a>
+            <h2 class="sectiontopic">INSTANT CASH FOR <br> YOUR <b><i><span class="gold">GOLD!</span></i></b> </h2>
+            <h3 class="registration">New Here ?</h3>
+            <h3 class="reg-txt">Sign up and discover great amount of new opportunities!</h3>
+            <a href="<?php echo URLROOT ?>/Users/signup"> <button class="signup"> Sign Up >></button></a>
 
-    </div>
+        </div>
 
-    <div class="info">
-      <div class="sec">
-        <img src="<?php echo URLROOT ?>/img/icons8-lock-100.png" alt="" class="pic secure">
-        <h3 class="txt">Safe & Secure!</h3>
-      </div>
+        <div class="info">
+            <div class="sec">
+                <img src="<?php echo URLROOT ?>/img/icons8-lock-100.png" alt="" class="pic secure">
+                <h3 class="txt">Safe & Secure!</h3>
+            </div>
 
-      <div class="sec">
-        <img src="<?php echo URLROOT ?>/img/icons8-magnetic-card-100.png" alt="" class="pic payment">
-        <h3 class="txt">Anytime Anywhere Payment</h3>
-      </div>
-      <div class="sec">
-        <img src="<?php echo URLROOT ?>/img/icons8-golds-64.png" alt="" class="pic rate">
-        <h3 class="txt">Best Gold Rates</h3>
-      </div>
-      <!-- <div class="process">
+            <div class="sec">
+                <img src="<?php echo URLROOT ?>/img/icons8-magnetic-card-100.png" alt="" class="pic payment">
+                <h3 class="txt">Anytime Anywhere Payment</h3>
+            </div>
+            <div class="sec">
+                <img src="<?php echo URLROOT ?>/img/icons8-golds-64.png" alt="" class="pic rate">
+                <h3 class="txt">Best Gold Rates</h3>
+            </div>
+            <!-- <div class="process">
   <!-- <img class="logo 100" src="<?php echo URLROOT ?>/img/sanda-01.png"> -->
-      <!-- </div> -->
+            <!-- </div> -->
 
 
-    </div>
-  </section>
-  <!-- Header End -->
-  <section class="section-services">
-    <h2 class="sevices-txt"> Our Services</h2>
-    <div class="service-info">
-
-      <div>
-        <div class="card">
-          <img src="<?php echo URLROOT ?>/img/dmitry-demidko-eBWzFKahEaU-unsplash.jpg" alt="" class="service-pic">
-          <p class="header-service">
-            Instant Cash For Gold
-          </p>
         </div>
-      </div>
+    </section>
+    <!-- Header End -->
+    <section class="section-services">
+        <h2 class="sevices-txt"> Our Services</h2>
+        <div class="service-info">
 
-      <div>
-        <div class="card">
-          <img src="<?php echo URLROOT ?>/img/scottsdale-mint-IYg1hjCO5DA-unsplash.jpg" alt="" class="service-pic">
-          <p class="header-service">
-            Gold Article Validation
-          </p>
-        </div>
-      </div>
-
-      <div>
-        <div class="card">
-          <img src="<?php echo URLROOT ?>/img/vaibhav-nagare-vv2vIFeNEMg-unsplash.jpg" alt="" class="service-pic">
-          <p class="header-service">
-            Release Pawned Articles
-          </p>
-        </div>
-      </div>
-      <div>
-        <div class="card">
-          <img src="<?php echo URLROOT ?>/img/Screenshot 2023-01-31 235407.png" alt="" class="service-pic">
-          <p class="header-service">
-            Secure Gold Locker Facility
-          </p>
-        </div>
-      </div>
-    </div>
-  </section>
-  <section class="section-about">
-    <h2 class="sevices-txt"> About</h2>
-
-
-
-    <div class="projcard-container">
-
-      <div class="projcard projcard-1">
-        <div class="projcard-innerbox">
-          <img class="projcard-img" src="<?php echo URLROOT ?>/Img/jp-valery-lVFoIi3SJq8-unsplash.jpg" />
-          <div class="projcard-textbox">
-            <div class="projcard-title">WE BUY YOUR GOLD!</div>
-            <div class="projcard-bar"></div>
-            <div class="projcard-description"><b>S</b>pecializes in buying gold at the best market price;
-              with our expertisise we are able to value and make the whole process of selling gold transparent. </div>
-
-          </div>
-        </div>
-      </div>
-
-      <div class="projcard projcard-2">
-        <div class="projcard-innerbox">
-          <img class="projcard-img" src="<?php echo URLROOT ?>/Img/maria-ziegler-jJnZg7vBfMs-unsplash.jpg" />
-          <div class="projcard-textbox">
-            <div class="projcard-title">WE SECURE YOUR GOLD !</div>
-            <div class="projcard-bar"></div>
-            <div class="projcard-description">
-              <b>A</b>nxious about the security of your gold? Worry no further. Within our vaults we you provide the best security for your gold assets.
+            <div>
+                <div class="card">
+                    <img src="<?php echo URLROOT ?>/img/dmitry-demidko-eBWzFKahEaU-unsplash.jpg" alt=""
+                        class="service-pic">
+                    <p class="header-service">
+                        Instant Cash For Gold
+                    </p>
+                </div>
             </div>
-            <div class="projcard-tagbox">
+
+            <div>
+                <div class="card">
+                    <img src="<?php echo URLROOT ?>/img/scottsdale-mint-IYg1hjCO5DA-unsplash.jpg" alt=""
+                        class="service-pic">
+                    <p class="header-service">
+                        Gold Article Validation
+                    </p>
+                </div>
             </div>
-          </div>
+
+            <div>
+                <div class="card">
+                    <img src="<?php echo URLROOT ?>/img/vaibhav-nagare-vv2vIFeNEMg-unsplash.jpg" alt=""
+                        class="service-pic">
+                    <p class="header-service">
+                        Release Pawned Articles
+                    </p>
+                </div>
+            </div>
+            <div>
+                <div class="card">
+                    <img src="<?php echo URLROOT ?>/img/Screenshot 2023-01-31 235407.png" alt="" class="service-pic">
+                    <p class="header-service">
+                        Secure Gold Locker Facility
+                    </p>
+                </div>
+            </div>
         </div>
-      </div>
+    </section>
+    <section class="section-about">
+        <h2 class="sevices-txt"> About</h2>
 
 
-    </div>
 
-    <div class="how-section">
-      <div class="howdiv">
-        <h2 class="how-h2">HOW IT WORKS</h2>
-        <h3 class="how-h3">Simple, Safe and Secure steps to pawn your gold!</h3>
-        <div class="step-content">
-          <div class="step">
-            <h3 class="how-h3">Step 01</h3>
-            <img src="<?php echo URLROOT ?>/Img/icons8-charity-64.png" alt="" class="step-img">
-            <h3 class="how-txt">Register With Us<br>It's only few clicks away</h3>
-          </div>
-          <div class="step">
-            <h3 class="how-h3">Step 02</h3>
-            <img src="<?php echo URLROOT ?>/Img/icons8-today-64.png" alt="" class="step-img">
-            <h3 class="how-txt">Make a Appointment <br>or walk in to a vogue outlet</h3>
-          </div>
-          <div class="step">
-            <h3 class="how-h3">Step 03</h3>
-            <img src="<?php echo URLROOT ?>/Img/icons8-scales-64.png" alt="" class="step-img">
-            <h3 class="how-txt">We weight and test the purity of your gold.</h3>
-          </div>
-          <div class="step">
-            <h3 class="how-h3">Step 04</h3>
-            <img src="<?php echo URLROOT ?>/Img/icons8-investing-64.png" alt="" class="step-img">
-            <h3 class="how-txt">Once the gold is tested and valued, we will make you an offer.</h3>
-          </div>
-          <div class="step">
-            <h3 class="how-h3">Step 05 
-            </h3>
-            <img src="<?php echo URLROOT ?>/Img/icons8-money-64.png" alt="" class="step-img">
-            <h3 class="how-txt">Get cash instantly at our branch</h3>
-          </div>
+        <div class="projcard-container">
+
+            <div class="projcard projcard-1">
+                <div class="projcard-innerbox">
+                    <img class="projcard-img" src="<?php echo URLROOT ?>/Img/jp-valery-lVFoIi3SJq8-unsplash.jpg" />
+                    <div class="projcard-textbox">
+                        <div class="projcard-title">WE BUY YOUR GOLD!</div>
+                        <div class="projcard-bar"></div>
+                        <div class="projcard-description"><b>S</b>pecializes in buying gold at the best market price;
+                            with our expertisise we are able to value and make the whole process of selling gold
+                            transparent. </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="projcard projcard-2">
+                <div class="projcard-innerbox">
+                    <img class="projcard-img" src="<?php echo URLROOT ?>/Img/maria-ziegler-jJnZg7vBfMs-unsplash.jpg" />
+                    <div class="projcard-textbox">
+                        <div class="projcard-title">WE SECURE YOUR GOLD !</div>
+                        <div class="projcard-bar"></div>
+                        <div class="projcard-description">
+                            <b>A</b>nxious about the security of your gold? Worry no further. Within our vaults we you
+                            provide the best security for your gold assets.
+                        </div>
+                        <div class="projcard-tagbox">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
-      </div>
-      <img class="howImg" src="<?php echo URLROOT ?>/img/lum3n-rtAEbQ6RrgI-unsplash.jpg">
 
-    </div>
-  </section>
-  <section class="FAQ">
-  <h2 class="sevices-txt"> FAQ</h2>
+        <div class="how-section">
+            <div class="howdiv">
+                <h2 class="how-h2">HOW IT WORKS</h2>
+                <h3 class="how-h3">Simple, Safe and Secure steps to pawn your gold!</h3>
+                <div class="step-content">
+                    <div class="step">
+                        <h3 class="how-h3">Step 01</h3>
+                        <img src="<?php echo URLROOT ?>/Img/icons8-charity-64.png" alt="" class="step-img">
+                        <h3 class="how-txt">Register With Us<br>It's only few clicks away</h3>
+                    </div>
+                    <div class="step">
+                        <h3 class="how-h3">Step 02</h3>
+                        <img src="<?php echo URLROOT ?>/Img/icons8-today-64.png" alt="" class="step-img">
+                        <h3 class="how-txt">Make a Appointment <br>or walk in to a vogue outlet</h3>
+                    </div>
+                    <div class="step">
+                        <h3 class="how-h3">Step 03</h3>
+                        <img src="<?php echo URLROOT ?>/Img/icons8-scales-64.png" alt="" class="step-img">
+                        <h3 class="how-txt">We weight and test the purity of your gold.</h3>
+                    </div>
+                    <div class="step">
+                        <h3 class="how-h3">Step 04</h3>
+                        <img src="<?php echo URLROOT ?>/Img/icons8-investing-64.png" alt="" class="step-img">
+                        <h3 class="how-txt">Once the gold is tested and valued, we will make you an offer.</h3>
+                    </div>
+                    <div class="step">
+                        <h3 class="how-h3">Step 05
+                        </h3>
+                        <img src="<?php echo URLROOT ?>/Img/icons8-money-64.png" alt="" class="step-img">
+                        <h3 class="how-txt">Get cash instantly at our branch</h3>
+                    </div>
+                </div>
+            </div>
+            <img class="howImg" src="<?php echo URLROOT ?>/img/lum3n-rtAEbQ6RrgI-unsplash.jpg">
+
+        </div>
+    </section>
+    <section class="FAQ">
+        <h2 class="sevices-txt"> FAQ</h2>
 
 
- 
-  </section>
-  <script src='<?php echo URLROOT ?>/js/landing.js'></script>
+
+
+        <div class="faq-div">
+
+            <label for="touch"><span class="span-title">titre</span></label>
+            <input type="checkbox" id="touch">
+
+            <ul class="slide">
+                <li class="faq-description"><a href="#">Lorem Ipsum</a></li>
+            </ul>
+
+        </div>
+
+
+
+    </section>
+    <script src='<?php echo URLROOT ?>/js/landing.js'></script>
 </body>
 
 </html>
