3a8c8bfe9af747bd3e885b4b1aea1c4df375b4f2
SandalikaSC <66319075+SandalikaSC@users.noreply.github.com>
2023-02-06 15:17:35 +0530

    payment page

--- app/views/Customer/Pawn-pay.php
+++ app/views/Customer/Pawn-pay.php
@@ -1,1 +1,27 @@
-awaa
+<?php require APPROOT . "/views/inc/header.php" ?>
+<link rel='stylesheet' type='text/css' media='screen' href='<?php echo URLROOT ?>/css/nav-bar.css'>
+
+<link rel='stylesheet' type='text/css' media='screen' href='<?php echo URLROOT ?>/css/Pawn-pay.css'>
+<title>Vogue | Payment</title>
+</head>
+
+<body class="wrapper">
+
+    <div class="">
+        <div class="right-heading">
+            <div class="right-side">
+                <a href="<?php echo URLROOT ?>/CustomerPawn" id="back">
+                    <img class="back" src="<?php echo URLROOT ?>/img/back.png" alt="back">
+                </a>
+                <h1 id="title">Payment</h1>
+            </div>
+            <img class="vogue" src="<?php echo URLROOT ?>/img/FULLlogo.png" alt="logo">
+        </div>
+
+    </div>
+    <div class="content">
+    </div>
+    <script>
+
+</script>
+<?php require APPROOT . "/views/inc/footer.php" ?>
