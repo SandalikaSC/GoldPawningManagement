355b3b2557d321faf3ab3c4c6f40aabf07704292
SandalikaSC <66319075+SandalikaSC@users.noreply.github.com>
2023-02-09 13:25:52 +0530

    Update allocateLocker.php

--- app/views/VaultKeeper/allocateLocker.php
+++ app/views/VaultKeeper/allocateLocker.php
@@ -1,5 +1,9 @@
 <?php require APPROOT . "/views/inc/header.php" ?>
+<<<<<<< Updated upstream
 <link rel='stylesheet' type='text/css' media='screen' href='<?php echo URLROOT ?>/css/VKPayment.css'>
+=======
+<link rel='stylesheet' type='text/css' media='screen' href='<?php echo URLROOT ?>/css/allocateLocker.css'>
+>>>>>>> Stashed changes
 <!-- <link rel='stylesheet' type='text/css' media='screen' href='<?php echo URLROOT ?>/css/article_pawn.css'> -->
 <title>Vogue | Allocate</title>
 </head>
@@ -19,6 +23,130 @@
 
     </div>
     <div class="content">
+<<<<<<< Updated upstream
         
+=======
+        <div class="locker-item">
+            <div class="jewellery-card">
+                <div class="jewellery-img">
+                     
+                    <img class="jw-img" src="<?php echo URLROOT ?>/img/harper-sunday-I89WziXZdVc-unsplash.jpg">
+                </div>
+                <div class="jw-details">
+                    <div class="jw-date">
+                        <div class="jw-date-name">
+                            <label>Estimated Value</label>
+                            <label class="jw-dt">Rs. 50000</label>
+                        </div>
+
+                    </div>
+                    <div class="jw-date-name">
+
+                        <label>Status</label>
+                        <label class="status tag-pending">Valid</label>
+                    </div>
+                    
+                </div>
+
+            </div>
+
+        </div>
+        <div class="item-details">
+            <!-- <div class="article-details"> -->
+            <div class="info-div">
+                <h2 class="sub-title">
+                    Customer Details
+                </h2>
+                <div class="jw-date">
+                    <div class="jw-date-name">
+                        <label>Customer Id</label>
+                        <label class="jw-dt">CU001</label>
+                    </div>
+
+                </div>
+                <div class="jw-date">
+                    <div class="jw-date-name">
+                        <label>Customer Name</label>
+                        <label class="jw-dt">Sandalika Chamari</label>
+                    </div>
+
+                </div>
+
+
+
+            </div>
+            <div class="due-payment info-div">
+                <h2 class="sub-title">
+                    Reservation Details
+                </h2>
+
+
+                <div class="jw-date">
+                    <div class="jw-date-name">
+                        <label>Reservatoin Date</label>
+                        <label class="jw-dt">2022/11/03</label>
+                    </div>
+
+                </div>
+
+                <div class="jw-date">
+                    <div class="jw-date-name">
+                        <label>Installment</label>
+                        <label class="jw-dt">Rs 12,500</label>
+                    </div>
+
+                </div>
+
+
+
+            </div>
+
+            <button class="pay-btn">Pay</button>
+
+
+            <!-- </div> -->
+
+        </div>
+        <div class="item-payments">
+            <!-- <div class="payment-history"> -->
+            <div class="payments his-div">
+                <h2 class="sub-title">
+                    Payment History </h2>
+                <div class="payments">
+                    <div class="pay-header">
+                        <label>Pay ID</label>
+                        <label>Inst No</label>
+                        <label>Paid Date</label>
+                        <label> Amount</label>
+
+                    </div>
+                    <div class="payment-content">
+                        <label>PD1956</label>
+                        <label>03</label>
+                        <label>2022/09/30 09:53 A.M.</label>
+                        <label>Rs. 5000</label>
+
+                    </div>
+                    <div class="payment-content">
+                        <label>PD1866</label>
+                        <label>02</label>
+                        <label>2022/09/30 09:53 A.M.</label>
+                        <label>Rs.25000</label>
+
+                    </div>
+                    <div class="payment-content">
+                        <label>PD1096</label>
+                        <label>01</label>
+                        <label>2022/09/30 09:53 A.M.</label>
+                        <label>Rs. 15000</label>
+
+                    </div>
+
+                </div>
+            </div>
+
+
+        </div>
+>>>>>>> Stashed changes
     </div>
     <?php require APPROOT . "/views/inc/footer.php" ?>
