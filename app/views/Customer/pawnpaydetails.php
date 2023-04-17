bd6af0186324ca261a677baed9d94c50de6fccb2
SandalikaSC <66319075+SandalikaSC@users.noreply.github.com>
2023-04-17 16:19:42 +0530

    extend payment

--- app/views/Customer/pawnpaydetails.php
+++ app/views/Customer/pawnpaydetails.php
@@ -88,7 +88,7 @@
                             <label for="yes-button" class="button-label">
                                 <h1>Yes</h1>
                             </label>
-                            <input type="radio" name="accept-offers" id="no-button" value="2" class="hidden radio-label">
+                            <input type="radio" name="accept-offers" id="no-button" value="2" class="hidden radio-label" checked>
                             <label for="no-button" class="button-label">
                                 <h1>No</h1>
                             </label>
@@ -99,95 +99,110 @@
                     </div>
                 </div>
             </div>
-           
-                <div class="info-div choise-div" id="extend">
-                    <h2 class="sub-title">
-                        Extend Duration
-                    </h2>
-                    <div class="jw-date">
-                        <div class="jw-date-name">
-                            <label>Reserve locker till</label>
-                            <input type="date" name="date" id="date" class="datechooser" value="">
-                        </div>
 
+            <div class="info-div choise-div" id="extend">
+                <h2 class="sub-title">
+                    Extend Duration
+                </h2>
+                <div class="jw-date">
+                    <div class="jw-date-name">
+                        <label>Reserve locker till</label>
+                        <input type="date" name="date" id="date" class="datechooser" value="">
                     </div>
-                    <div class="jw-date">
-                        <div class="jw-date-name">
-                            <label>Extend payment</label>
-                            <label class="jw-dt"><?= $data['reservationId'] ?></label>
-                        </div>
-
-                    </div>
-                    <div class="jw-date">
-                        <div class="jw-date-name">
-                            <label class="err" id=extend-err"> fefew</label>
-                            <button type="button" name="date" id="date" class="btn-extend" value="Extend">Extend</button>
-                        </div>
-
-                    </div>
-
 
                 </div>
-                <div class="info-div choise-div" id="appointment">
-                    <h2 class="sub-title">
-                        Retrive Appointment
-                    </h2>
-                    <div class="jw-date">
-                        <div class="jw-date-name">
-                            <label>Reservation Id</label>
-                            <label class="jw-dt"><?= $data['reservationId'] ?></label>
-                        </div>
-
+                <div class="jw-date">
+                    <div class="jw-date-name">
+                        <label>Extend payment</label>
+                        <label class="jw-dt"><?= $data['reservationId'] ?></label>
                     </div>
-                    <div class="jw-date">
-                        <div class="jw-date-name">
-                            <label>Article Id</label>
-                            <label class="jw-dt"><?= $data['articleId'] ?></label>
-                        </div>
 
-                    </div>
-                    <div class="jw-date">
-                        <div class="jw-date-name">
-                            <label>Reserved interest</label>
-                            <label class="jw-dt"><?= $data['interest']->Interest_Rate . '%' ?></label>
-                        </div>
-
-                    </div>
-                    <div class="jw-date">
-                        <div class="jw-date-name">
+                </div>
+                <div class="jw-date">
+                    <div class="jw-date-name">
                         <label class="err" id=extend-err"> fefew</label>
-                        <button type="button" name="date" id="date" class="btn-extend" value="Extend">Add</button>
-                        </div>
-
+                        <button type="button" name="date" id="date" class="btn-extend" value="Extend">Extend</button>
                     </div>
- 
 
                 </div>
-            
+
+
+            </div>
+            <div class="info-div choise-div" id="appointment">
+                <h2 class="sub-title">
+                    Retrive Appointment
+                </h2>
+                <div class="jw-date">
+                    <div class="jw-date-name">
+                        <label>Appointment Date</label>
+                        <input type="date" name="date" id="appointment_date" class="datechooser" value="" onchange="handleDateSelection()">
+                    </div>
+
+                </div>
+                <div class="jw-date">
+                    <div class="jw-date-name time-slots" id="time-slots">
+
+                        <?php foreach ($data['timeslot'] as $timeslot) : ?>
+                            <div class=" selecotr-item">
+                                <input type="radio" value="<?php echo $timeslot->slotID ?>" id=<?php echo $timeslot->slotID ?> name="selector" class=" selector-item_radio" checked>
+                                <label for=<?php echo $timeslot->slotID ?> class="selector-item_label">
+                                    <?php echo $timeslot->time ?>
+                                </label>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+
+                </div>
+
+                <div class="jw-date">
+                    <div class="jw-date-name">
+                        <label class="err" id=extend-err"> </label>
+                        <button type="button" name="date" id="date" class="btn-extend" value="Extend" onclick="updatePaymnet()">Add</button>
+                    </div>
+
+                </div>
+
+
+            </div>
+
             <div class="payment">
                 <h2>
                     Payment Details
                 </h2>
                 <div class="sec">
                     <label for="Due">Due Pay</label>
-                    <label class="Due-pay" for="Total">Rs. 1200.00</label>
+                    <label class="Due-pay" id="fine_pay" for="Total"> Rs. <?= number_format($data['duedays'] * $data['fineRate']->Interest_Rate, 2); ?></label>
+                </div>
+                <div class="sec">
+                    <label for="Total">Extend Pay</label>
+                    <label class="Tot-pay" id="extend_pay" for="Total">Rs. 00.00</label>
                 </div>
                 <div class="sec">
                     <label for="Total">Total Pay</label>
-                    <label class="Tot-pay" for="Total">Rs. 6200.00</label>
+                    <label class="Tot-pay"  id="total_pay" for="Total">Rs. 6200.00</label>
                 </div>
                 <div class=" sec-btn">
-                    <a class="p-btn cancel">Cancel</button></a>
-                    <a class="p-btn pay">Pay</button></a>
+                    <a class="p-btn ">Cancel</button></a>
+                    <a class="p-btn ">Pay</button></a>
                 </div>
             </div>
 
         </div>
+        <script src="<?php echo URLROOT ?>/js/jquery.min.js"></script>
         <script>
+            var today = new Date();
+            var tomorrow = new Date(today.getTime() + (24 * 60 * 60 * 1000));
+            var year = tomorrow.getFullYear();
+            var month = (tomorrow.getMonth() + 1).toString().padStart(2, "0");
+            var day = tomorrow.getDate().toString().padStart(2, "0");
+            var tomorrowFormatted = year + "-" + month + "-" + day;
+
+            document.getElementById("appointment_date").value = tomorrowFormatted;
+
             const yes = document.getElementById("yes-button");
             const no = document.getElementById("no-button");
 
-            document.getElementById("appointment").classList.add("disabledbutton");
+
             document.getElementById("extend").classList.add("disabledbutton");
             yes.addEventListener("click", function(event) {
                 document.getElementById("extend").classList.add("flexbutton");
@@ -202,5 +217,58 @@
                 document.getElementById("extend").classList.add("disabledbutton");
                 document.getElementById("extend").classList.remove("flexbutton");
             });
+
+
+            function handleDateSelection() {
+
+                var selectedDate = document.getElementById("appointment_date").value;
+
+                $.ajax({
+                    type: "POST",
+                    url: "<?= URLROOT ?>/CustomerLocker/getTimeSlots",
+                    data: {
+                        date: selectedDate
+                    },
+                    dataType: "JSON",
+                    success: function(response) {
+                        var parentElement = document.getElementById("time-slots");
+                        while (parentElement.firstChild) {
+                            parentElement.removeChild(parentElement.firstChild);
+                        }
+
+                        response.forEach(function(item) {
+
+                            var newElement = '<div class="selector-item">' +
+                                '<input type="radio" value="' + item.slotID + ' "id="' + item.slotID + '" name="selector" class="selector-item_radio" checked>' +
+                                '<label  for="' + item.slotID + '" class="selector-item_label">' + item.time + '</label>' +
+                                '</div>';
+                            $("#time-slots").append(newElement);
+                        });
+
+
+                    },
+                    error: function(xhr, status, error) {
+                        console.log("Error: " + error);
+                    }
+                });
+            }
+
+            function updatePaymnet() {
+                var selectedDate = document.getElementById("appointment_date").value;
+                var today = new Date();
+
+                // create a Date object for a specific date
+                var otherDate = new Date(selectedDate);
+
+                // calculate the difference in milliseconds
+                var differenceMs = otherDate.getTime() - today.getTime();
+
+                // convert the difference to days
+                var differenceDays = Math.ceil(differenceMs / (1000 * 60 * 60 * 24));
+
+
+                
+
+            }
         </script>
         <?php require APPROOT . "/views/inc/footer.php" ?>
