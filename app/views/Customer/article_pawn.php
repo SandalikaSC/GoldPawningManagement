eb8af6067a5b2338ab998c41079ecb7216749a23
SandalikaSC <66319075+SandalikaSC@users.noreply.github.com>
2023-05-14 13:58:16 +0530

    Update article_pawn.php

--- app/views/Customer/article_pawn.php
+++ app/views/Customer/article_pawn.php
@@ -176,9 +176,7 @@
         </div>
         <div class="payment-history">
 
-            <?php if ($data['status'] != 'Auctioned' && $data['status'] != 'Retrieved' && $data['status'] != 'Completed') : ?>
-                <a class="a-pay" href="<?php echo URLROOT ?>/CustomerPawn/makePayment/<?= $data['goldLoan']->Pawn_Id ?>"  >
-
+           
             <?php if ($data['status'] != 'Auctioned' && $data['status'] != 'Retrieved' && $data['status'] != 'Completed' && $data['status'] != 'Repawn') : ?>
                 <a class="a-pay" href="<?php echo URLROOT ?>/CustomerPawn/makePayment/<?= $data['goldLoan']->Pawn_Id ?>">
 
